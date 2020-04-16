6965ddb0a2dabcd03328e78d1aa8c714cff4b894
rjurado01 <rjurado01@gmail.com>
2020-04-16 20:56:43 +0200

    Login with Pow.

---
+++ lib/app_web/router.ex
@@ -0,0 +1,34 @@
+defmodule AppWeb.Router do
+  use AppWeb, :router
+  use Pow.Phoenix.Router
+
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/" do
+    pipe_through :browser
+
+    pow_routes()
+  end
+
+  scope "/", AppWeb do
+    pipe_through :browser
+
+    get "/", PageController, :index
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", AppWeb do
+  #   pipe_through :api
+  # end
+end
